ffc660a04ef62374f5c53225f9bfcb795c852c8e
paranoidcake <victorpanayotov@gmail.com>
2019-02-09 20:25:08 +0000

    Added a ;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SubjectComponent } from './subject/subject.component';
 
-import { FlexLayoutModule } from '@angular/flex-layout'
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
